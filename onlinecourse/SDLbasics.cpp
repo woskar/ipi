e444e460f6c2426764d635e21c999db7a8630f3d
Oskar Weinfurtner <oskar.weinfurtner@icloud.com>
2017-04-03 21:54:12 +0200

    running version of SDL

--- onlinecourse/SDLbasics.cpp
+++ onlinecourse/SDLbasics.cpp
@@ -1,104 +1,51 @@
-
-// here is my sdl2 saved:
-// /usr/local/Cellar/sdl2/2.0.5/lib
-// /usr/local/Cellar/sdl2/2.0.5/include
-// compile: g++ Main.cpp -o Main -I/home/user/SDL/include/SDL2
-
-/*
-Works so far:
-g++ SDLbasics.cpp -o sdl -I/usr/local/Cellar/sdl2/2.0.5/include/SDL2 -D_THREAD_SAFE -L/usr/local/lib -lSDL2
-*/
-
+// run with:
+// g++ SDLbasics.cpp -o sdl -I/usr/local/Cellar/sdl2/2.0.5/include/SDL2 -D_THREAD_SAFE -L/usr/local/lib -lSDL2
 
 #include <iostream>
 #include <stdio.h>
+#include <iomanip>
 #include <SDL.h>
+#include <math.h>
 #include "Screen.h"
 using namespace std;
 
 int main()
 {
-    if(SDL_Init(SDL_INIT_VIDEO)<0)
-    {
-        cout << "SDL_init failed." << endl;
-        return 1;
-    }
 
-    SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion",
-            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-            SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    // create a Screen object
+    Screen screen;
 
-    if(window == NULL)
-    {
-        SDL_Quit();
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 2;
-    }
+    if(screen.init() == false)
+        cout << "Error initializing SDL." << endl;
 
-    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-    SDL_Texture * texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    if(renderer == NULL)
-    {
-        cout << "Could not create renderer" << endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 3;
-    }
 
-    if(texture == NULL)
-    {
-        cout << "Could not create texture" << endl;
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 4;
-    }
-
-    Uint32 * buffer = new Uint32[SCREEN_WIDTH*SCREEN_HEIGHT];
-
-    // make window white by setting all bytes in buffer to maximum value (255 = 0xFF)
-    // if value is 0, then screen is black.
-    memset(buffer, 0, SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(Uint32));
-
-    // set individual pixel to white
-    buffer[333333] = 0xFFFFFFFF; // two f's for each red, green, blue, alpha
-
-    // set whole screen to certain color by looping over pixels:
-    for(int i=0; i<SCREEN_WIDTH*SCREEN_HEIGHT; ++i)
-    {
-        buffer[i] = 0xFFFF00FF;
-    }
-
-    // Drawing Code:
-    SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH*sizeof(Uint32));
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, texture, NULL, NULL);
-    SDL_RenderPresent(renderer);
-
-    bool quit = false;
-    SDL_Event event;
-    while(!quit)
+    while(true)
     {
         // Update particles
-        // Draw particles
-        // Check for messages/events
 
-        while(SDL_PollEvent(&event))
+        // Draw particles
+        for(int y=0; y < Screen::SCREEN_HEIGHT; y++)
         {
-            if(event.type == SDL_QUIT)
+            for(int x=0; x < Screen::SCREEN_WIDTH; x++)
             {
-                quit = true;
+                screen.setPixel(x, y, 128, 0, 255);
             }
         }
+        screen.setPixel(400, 300, 255, 255, 255);
+
+        screen.setPixel(200, 300, 255, 255, 255);
+
+        // Draw the screen
+        screen.update();
+
+        // Check for messages/events
+        if(screen.processEvents() == false)
+            break;
     }
-    delete[] buffer;
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyTexture(texture);
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-
+    screen.close();
     return 0;
+
+
 }
--- onlinecourse/Screen.h
+++ onlinecourse/Screen.h
@@ -3,6 +3,10 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
 class Screen
 {
 public:
@@ -27,20 +31,112 @@ public:
     // True if init succeeds
     bool init()
     {
+        if(SDL_Init(SDL_INIT_VIDEO) < 0)
+        {
+            cout << "SDL_init failed." << endl;
+            return false;
+        }
+
+        m_window = SDL_CreateWindow("Particle Fire Explosion",
+                SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+        if(m_window == NULL)
+        {
+            SDL_Quit();
+            printf("Could not create window: %s\n", SDL_GetError());
+            return false;
+        }
+
+        m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_PRESENTVSYNC);
+        m_texture = SDL_CreateTexture(m_renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+        if(m_renderer == NULL)
+        {
+            cout << "Could not create renderer" << endl;
+            SDL_DestroyWindow(m_window);
+            SDL_Quit();
+            return false;
+        }
+
+        if(m_texture == NULL)
+        {
+            cout << "Could not create texture" << endl;
+            SDL_DestroyRenderer(m_renderer);
+            SDL_DestroyWindow(m_window);
+            SDL_Quit();
+            return false;
+        }
+
+        m_buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+
+        // make window white by setting all bytes in buffer to maximum value (255 = 0xFF)
+        // if value is 0, then screen is black.
+        //memset(m_buffer, 0xFF, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+
+
+        // set individual pixel to white
+        //m_buffer[2403] = 0xFFFFFFFF; // two f's for each red, green, blue, alpha
+    /*
+        // set whole screen to certain color by looping over pixels:
+        for(int i=0; i<SCREEN_WIDTH*SCREEN_HEIGHT; ++i)
+            m_buffer[i] = 0xFFFF00FF;
+    */
         return true;
+    }
+
+
+    // set Pixel at xy-Coordinate with color
+    void setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue)
+    {
+        Uint32 color = 0;
+        // compose Hex-Color 0xrrggbbaa by adding and bitshift
+        color += red;
+        color <<= 8;
+        color += green;
+        color <<= 8;
+        color += blue;
+        color <<= 8;
+        color += 0xFF; //for alpha value
+
+        // go to right pixel and set color
+        m_buffer[(y * SCREEN_WIDTH) + x] = color;
+    }
+
+    // updating the screen
+    void update()
+    {
+        // Drawing Code, putting pixel data to screen:
+        SDL_UpdateTexture(m_texture, NULL, m_buffer, SCREEN_WIDTH * sizeof(Uint32));
+        SDL_RenderClear(m_renderer);
+        SDL_RenderCopy(m_renderer, m_texture, NULL, NULL);
+        SDL_RenderPresent(m_renderer);
     }
 
     // Eventprocessing
     bool processEvents()
     {
-        return false;
+        SDL_Event event;
+        // checks all the events, if any are Quit, so quit.
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_QUIT)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     // Deallocation/Close-down function
-    void close();
-
-
-
+    void close()
+    {
+        delete[] m_buffer;
+        SDL_DestroyRenderer(m_renderer);
+        SDL_DestroyTexture(m_texture);
+        SDL_DestroyWindow(m_window);
+        SDL_Quit();
+    }
 };
 
 #endif
